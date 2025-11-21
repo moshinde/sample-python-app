2b66781e33485dea2c0f1ec58cca8d978848a9c0
Aniket Jadhav <jadhavaniket008@gmail.com>
2025-11-21 16:30:11 +0530

    check api

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import socket
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "App deployed using App Service Build"}
+    hostname = socket.gethostname()
+    return {
+        "message": "App deployed using App Service Build",
+        "hostname": hostname
+    }
 
 @app.get("/api/data")
-def get_data():
-    return {"status": "success", "value": 1234}
+def get_data(request: Request):
+    # Client IP
+    client_ip = request.client.host
+
+    # Server hostname
+    hostname = socket.gethostname()
+
+    return {
+        "status": "success",
+        "value": 1234,
+        "client_ip": client_ip,
+        "server_hostname": hostname
+    }
