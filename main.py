67c1514987701abf9cb71a019699ca230193da9e
Aniket Jadhav <jadhavaniket008@gmail.com>
2025-11-24 15:25:27 +0530

    check api

--- main.py
+++ main.py
@@ -13,15 +13,25 @@ def root():
 
 @app.get("/api/data")
 def get_data(request: Request):
-    # Client IP
     client_ip = request.client.host
-
-    # Server hostname
     hostname = socket.gethostname()
 
     return {
         "status": "success",
         "value": 1234,
+        "client_ip": client_ip,
+        "server_hostname": hostname
+    }
+
+# NEW: For testing Application Gateway path rules
+@app.get("/test/appgw")
+def test_appgw(request: Request):
+    client_ip = request.client.host
+    hostname = socket.gethostname()
+
+    return {
+        "route": "/test/appgw",
+        "message": "Application Gateway path rule working!",
         "client_ip": client_ip,
         "server_hostname": hostname
     }
